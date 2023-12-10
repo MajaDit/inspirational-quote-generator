aefd0827064c61e65ff761df11d025e780e6b242
Maja Dit <maja.ditrtova@outlook.com>
2023-12-10 18:44:08 +0000

    implement the ai api, and fix typo in ID

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+const displayQuote = (response) => {
+  new Typewriter("#quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+};
 
+const generateQuote = (event) => {
+  event.preventDefault();
+
+  let userInput = document.getElementById("user-input");
+  let apiKey = "7b34357f3et00d865954fab83029o0ae";
+  let context =
+    "You are an expert in motivational speaking that loves to write short inspirational quotes. Your mission is to generate no more than 4-lines inspirational quote. Sign every quote you generate with 'SheCodes AI' at the end of the quote. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate an inspirational quote about ${userInput}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.getElementById("quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="dot-pulse"></div>`;
+  axios.get(apiURL).then(displayQuote);
+};
+
+let quoteFormElement = document.getElementById("quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
